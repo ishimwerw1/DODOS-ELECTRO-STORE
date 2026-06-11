e0175c8dcf584b71da016266e39bae75335d6de6
ishimwerw1 <ishimwerda@gmail.com>
2026-06-11 18:21:27 +0200

    we are frontend deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // Get API URL from environment or fallback to local development URL
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:60000/api';
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  'https://dodos-electro-store.onrender.com/api';
 
 console.log('API URL being used:', API_URL); // Debugging line for console
 
